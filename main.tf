33becaddc567c4840cdea8ab599f8101034a4c18
meghasyam1997 <meghasyam01@gmail.com>
2023-06-29 14:07:29 +0530

    tags to vpc

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support = true
 
-  tags = merge(var.tags,{Name = "${var.dev}-vpc"})
+  tags = merge(var.tags,{Name = "${var.env}-vpc"})
 
 }
